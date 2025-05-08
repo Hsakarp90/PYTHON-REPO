2c9a7fd1632c57be7ad105090a6f09a3d6f94685
Hsakarp90 <mithun90147@gmali.com>
2025-05-09 01:46:40 +0530

    just a test

--- basic/function.py
+++ basic/function.py
@@ -8,3 +8,13 @@ num3 = int(input("Enter third number: "))
 
 sum_result = add_three_numbers(num1, num2, num3)
 print("The sum is:", sum_result)
+def add_three_numbers(a, b, c):
+    return a + b + c
+
+# Example usage:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+sum_result = add_three_numbers(num1, num2, num3)
+print("The sum is:", sum_result)
