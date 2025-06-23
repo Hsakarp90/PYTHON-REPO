7750dbbadbee3d2f11868ff000e77e16f9f54f32
Om prakash parida <mithun90147@gmail.com>
2025-06-23 16:54:00 +0530

    i just creat a new folder

--- basic/Inputprogram.py
+++
@@ -1,6 +0,0 @@
-# Taking input from the user
-user_name = input("Enter your name: ")
-user_age = input("Enter your age: ")
-
-# Displaying the input back to the user
-print(f"Hello, {user_name}! You are {user_age} years old.")
